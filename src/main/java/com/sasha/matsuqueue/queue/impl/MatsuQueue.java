0eeea748877ff0b1974daef3fc0f5408c8647e1f
EmotionalLove <sashwho@vivaldi.net>
2019-06-02 22:55:45 -0700

    logging

--- src/main/java/com/sasha/matsuqueue/queue/impl/MatsuQueue.java
+++ src/main/java/com/sasha/matsuqueue/queue/impl/MatsuQueue.java
@@ -8,6 +8,7 @@ import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 import java.util.UUID;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.logging.Level;
 
 public class MatsuQueue implements IMatsuQueue {
 
@@ -28,6 +29,7 @@ public class MatsuQueue implements IMatsuQueue {
     public void addPlayerToQueue(ProxiedPlayer player) {
         queue.offer(player.getUniqueId());
         player.sendMessage(new TextComponent(Matsu.CONFIG.serverFullMessage.replace("&", "\247")));
+        Matsu.INSTANCE.getLogger().log(Level.INFO, player.getName() + " placed in queue " + this.name);
     }
 
     @Override
@@ -37,7 +39,9 @@ public class MatsuQueue implements IMatsuQueue {
 
     @Override
     public void connectFirstPlayerToDestinationServer() {
-        queue.poll();
+        ProxiedPlayer player = Matsu.INSTANCE.getProxy().getPlayer(queue.poll());
+        player.connect(Matsu.INSTANCE.getProxy().getServerInfo(Matsu.CONFIG.destinationServerKey));
+        Matsu.INSTANCE.getLogger().log(Level.INFO, player.getName() + " transferred to destination server");
     }
 
     @Override
